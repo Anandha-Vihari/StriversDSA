b272989e4ac38c3515925a6b174698c0c5973bd9
Anandha-Vihari <notevilnorgood@gmail.com>
2025-10-18 18:26:17 +0000

    Day 2

--- Patterns/8.DiamondStar.py
+++ Patterns/8.DiamondStar.py
@@ -1,14 +1,6 @@
 n=int(input())
 for i in range(1,n+1):
     for j in range(n-i):
-        print(" ", end="")
-    for k in range(2 * i - 1):
-        print("*", end="")
-    print()
-
-n=int(input())
-for i in range(1,n):
-    for j in range(n-i):
         print(" ",end="")
     for k in range(2*i-1):
         print("*",end="")
@@ -16,3 +8,11 @@ for i in range(1,n):
         print(" ",end="")
     print()
 
+for i in range(n-1,0,-1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(2*i-1):
+        print("*",end="")
+    for l in range(n-i):
+        print(" ",end="")
+    print()
